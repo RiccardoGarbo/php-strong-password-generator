6e7548102cfec6e400c18cb5985b8edd3f355992
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-02-21 17:48:04 +0100

    create new file for function and include on main file

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
-<?php 
-$pass_length = $_GET['pass_length'];
 
-$generate_password = function($pass_length){ 
-    $pass_array = [];
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=[]{}|;:,.<>?';
-    for ( $i = 0 ; $i <= $pass_length; $i++){
-        $random_characters = rand(0, strlen($characters) - 1);
-        $pass_array[] = $characters[$random_characters];       
-    };
-    return implode('', $pass_array);
-}
+<?php 
+include __DIR__ . '/function.php';
+
 ?>
-<?php $password = $generate_password($pass_length);?>
 
 
 
